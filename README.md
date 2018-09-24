881dda539dc0f3df1f996d6bf47dbffa72f68f66
fastsystem <fastsystem>
2018-09-24 23:13:49 +0900

    add Unity project and README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# unity-text-typing-animation
+
+Qiita
+
+[Unity Timeline でタイピングアニメーションみたいな事をする](https://qiita.com/kingyo222/items/9c65b962426a09cd4c45)
+
+Blog
+
+[Unity Timeline でタイピングアニメーションみたいな事をする](https://www.fast-system.jp/unity-timeline-%e3%81%a7%e3%82%bf%e3%82%a4%e3%83%94%e3%83%b3%e3%82%b0%e3%82%a2%e3%83%8b%e3%83%a1%e3%83%bc%e3%82%b7%e3%83%a7%e3%83%b3%e3%81%bf%e3%81%9f%e3%81%84%e3%81%aa%e4%ba%8b%e3%82%92%e3%81%99/)
---
+++ TextAnimeTest/Assets/Scenes/TextPlayableAsset.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+[System.Serializable]
+public class TextPlayableAsset : PlayableAsset
+{
+    public ExposedReference<GameObject> charaObj;
+    // public string text;
+
+    // Factory method that generates a playable based on this asset
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
+    {
+        var behaviour = new TextPlayableBehaviour();
+        behaviour.charaObject = charaObj.Resolve(graph.GetResolver());
+        // behaviour.text = text;
+        return ScriptPlayable<TextPlayableBehaviour>.Create(graph, behaviour);
+    }
+}
